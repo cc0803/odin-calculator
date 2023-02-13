84ee2e369b23943445b6bede942c5a0a438fcf2b
cc0803 <simongeldbach@gmail.com>
2023-02-13 17:55:33 +0100

    Create functions for Buttons, change Layout, first basic working version

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
-function checkNumber(a, b){
-    if (isNaN(a)|| isNaN(b)){
-        console.log("Incorrect input");
-        return false;
-    }
-    return true;
-}
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equals");
+const display = document.querySelector("#display");
+const clear = document.querySelector(".clear");
+
+let a = "";
+let b = "";
+let calculationOperator = "";
+let firstNumber = true;
+let result = "";
+
 
 function add(a,b){
-    if(checkNumber(a, b)){
-        return Number(a) + Number(b);
-    }
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b){
-    if(checkNumber(a, b)){
-        return Number(a) - Number(b);
-    }
+    return Number(a) - Number(b);
 }
 
 function multiply(a, b){
-    if (checkNumber(a, b)){
-        return Number(a) * Number(b);
-    }
+    return Number(a) * Number(b);
 }
 
 function divide(a, b){
-    if (checkNumber(a, b)){
-        return Math.round(Number(a) / Number(b) * 100) / 100;
-    }
+    return Math.round(Number(a) / Number(b) * 100) / 100;
+}
+
+function power(a, b){
+    return Number(a) ** Number(b);
 }
 
 function operate(operator, a, b){
@@ -44,7 +45,40 @@ function operate(operator, a, b){
         case "*":
             return multiply(a, b);
             break;
+        case "^":
+            return power(a, b);
         default: 
             console.log("Incorrect operator");
     }
 }
+
+// adding String together to get the number the user wished for
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if(firstNumber){
+            a += number.textContent;
+            display.textContent = a;
+        } else {
+            b += number.textContent;
+            display.textContent = `${calculationOperator}${b}`;
+        }
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        firstNumber = false;
+        calculationOperator = operator.textContent;
+    });
+});
+
+equal.addEventListener("click", () => {
+    result = operate(calculationOperator, a, b);
+    display.textContent = result;
+})
+
+clear.addEventListener("click", () => {
+    a = b = result = "";
+    firstNumber = true;
+    display.textContent = "";
+});
